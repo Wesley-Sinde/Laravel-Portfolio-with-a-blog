61e1714f141a31d1f0588c7ef917360663a74265
Wesley Sinde <sindewesley5@gmail.com>
2022-03-11 12:48:13 +0300

    Signed-off-by: Wesley Sinde <sindewesley5@gmail.com>

---
+++ resources/views/index.blade.php
@@ -0,0 +1,304 @@
+@extends('layouts.app')
+@section('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                <h1 class="sm:text-white text-2xl uppercase font-bold text-shadow-md pb-14 sm:text-2xl">
+                    Do you want to learn programming?
+                </h1>
+                <a href=""
+                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xs sm:text-xl hover:bg-green-300 uppercase">
+                    Yes i want to...
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="sm:grid grid-cols-2 gap-10  w-4/5 mx-auto py-5 border-gray-200 ">
+        <div>
+            <img src="{{ asset('images/Code.jpg') }}" alt="" width=100%>
+        </div>
+        <div class="m-auto sm:m-auto text-left w-4/5 block" style="padding-left: 2%">
+            <h2 class="text-xl font-extrabold text-gray-600">
+                Struggling to be a better web developer
+            </h2>
+            <p class="py-2 text-gray-500 text-xs">
+
+            </p>
+            <p class=" font-serif text-gray-600 text-s pb-9">
+                Technology plays a huge role in our daily lives, from the simplest of apps to the most groundbreaking
+                inventions. Every website or piece of software that we encounter has been built by a developer—but what
+                exactly is web development, and what do they do? All of this will be answered in this guide to how to become
+                a web developer.
+            </p>
+            <a class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl" href="/blog">
+                Find out more
+            </a>
+        </div>
+    </div>
+    <div class="text-center p-1 bg-black text-white">
+        <h2 class="text-2xl pb-5 text-l">
+            ABOUT ME
+        </h2>
+    </div>
+    <br><br>
+
+    <h1 align="center">Hi 👋, I'm Wesley Sinde</h1>
+    <h3 align="center">A passionate full stacks web developer from Nairobi, Kenya. I am a full-time Laravel developer.</h3>
+
+
+
+
+
+
+
+    <p align="left">Github <img
+            src="https://komarev.com/ghpvc/?username=wesley-sinde&label=Profile%20views&color=0e75b6&style=flat"
+            alt="wesley-sinde" /> </p>
+
+    <p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy">
+            <img src="https://github-profile-trophy.vercel.app/?username=wesley-sinde" alt="wesley-sinde" />
+        </a> </p>
+
+    <p align="left"> <a href="https://twitter.com/sindewesley" target="blank">
+            <img src="https://img.shields.io/twitter/follow/sindewesley?logo=twitter&style=for-the-badge"
+                alt="sindewesley" />
+        </a>
+    </p>
+    <br><br>
+    - 🌱 I’m currently learning React native. **React, Vue** <br><br>
+
+    - 👨‍💻 All of my projects are available at [https://github.com/Wesley-Sinde](https://github.com/Wesley-Sinde) <br><br>
+
+    - 📝 I regularly write articles on [https://www.wesley.io.ke/](https://www.wesley.io.ke/)
+    <br><br>
+    - 💬 Ask me about **React, Laravel, Php and Cpp**
+
+    <br><br>
+    - 📫 How to reach me **sindewesley5@gmail.com**
+    <br><br>
+    - 📄 Know about my experiences [https://www.wesley.io.ke/](https://www.wesley.io.ke/)
+    <br><br>
+    - ⚡ A character I have always wanted to meet.. **We’d all like to have coffee with Elon Musk and Mrs. Mark Zuckerberg**
+    <br><br>
+    <!-- BLOG-POST-LIST:START -->
+    <!-- BLOG-POST-LIST:END -->
+    <h3 class=" px-8 text-lg font-bold" align="left">Connect with me:</h3>
+    <div class="">
+        <p class="grid grid-cols-7 gap-10  w-4/5 mx-auto py-5 border-gray-200 " align="left">
+            <a href="https://dev.to/wesley-sinde" target="blank"><img align="left"
+                    src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/devto.svg"
+                    alt="wesley-sinde" height="30" width="40" /></a>
+            <a href="https://twitter.com/sindewesley" target="blank"><img align="left"
+                    src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg"
+                    alt="sindewesley" height="30" width="40" /></a>
+            <a href="https://linkedin.com/in/wesley sinde" target="blank"><img align="left"
+                    src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
+                    alt="wesley sinde" height="30" width="40" /></a>
+            <a href="https://stackoverflow.com/users/wesley-sinde" target="blank"><img align="left"
+                    src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/stack-overflow.svg"
+                    alt="wesley-sinde" height="30" width="40" /></a>
+            <a href="https://fb.com/wesley sinde" target="blank"><img align="left"
+                    src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg"
+                    alt="wesley sinde" height="30" width="40" /></a>
+            <a href="https://www.youtube.com/c/https://m.youtube.com/channel/ucqgjkmz6haifq-gihfwqifq" target="blank"><img
+                    align="left"
+                    src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/youtube.svg"
+                    alt="https://m.youtube.com/channel/ucqgjkmz6haifq-gihfwqifq" height="30" width="40" /></a>
+            <a href="https://www.codechef.com/users/wesley_001" target="blank"><img align="left"
+                    src="https://cdn.jsdelivr.net/npm/simple-icons@3.1.0/icons/codechef.svg" alt="wesley_001" height="30"
+                    width="40" /></a>
+        </p>
+    </div>
+
+    <h3 class=" px-8 text-lg font-bold" align="left">Languages and Tools:</h3>
+
+    <div>
+        <p class="grid grid-cols-12 gap-10  w-4/5 mx-auto py-5 border-gray-200 ">
+            <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer">
+                <img align="left" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
+                    alt="c" width="40" height="40" />
+            </a>
+            <a href="https://www.chartjs.org" target="_blank" rel="noreferrer">
+                <img align="left" src="https://www.chartjs.org/media/logo-title.svg" alt="chartjs" width="40" height="40" />
+            </a>
+            <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"
+                    alt="cplusplus" width="40" height="40" />
+            </a>
+            <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+                    alt="css3" width="40" height="40" />
+            </a>
+            <a href="https://www.docker.com/" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg"
+                    alt="docker" width="40" height="40" />
+            </a>
+            <a href="https://dotnet.microsoft.com/" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg"
+                    alt="dotnet" width="40" height="40" />
+            </a>
+            <a href="https://cloud.google.com" target="_blank" rel="noreferrer">
+                <img src="https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg" alt="gcp" width="40"
+                    height="40" />
+            </a>
+            <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+                    alt="html5" width="40" height="40" />
+            </a>
+            <a href="https://jasmine.github.io/" target="_blank" rel="noreferrer">
+                <img src="https://www.vectorlogo.zone/logos/jasmine/jasmine-icon.svg" alt="jasmine" width="40"
+                    height="40" />
+            </a>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                    alt="javascript" width="40" height="40" />
+            </a>
+            <a href="https://laravel.com/" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-plain-wordmark.svg"
+                    alt="laravel" width="40" height="40" />
+            </a>
+            <a href="https://mariadb.org/" target="_blank" rel="noreferrer">
+                <img src="https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg" alt="mariadb" width="40"
+                    height="40" />
+            </a>
+            <a href="https://mochajs.org" target="_blank" rel="noreferrer">
+                <img src="https://www.vectorlogo.zone/logos/mochajs/mochajs-icon.svg" alt="mocha" width="40" height="40" />
+            </a>
+            <a href="https://www.microsoft.com/en-us/sql-server" target="_blank" rel="noreferrer"> <img
+                    src="https://www.svgrepo.com/show/303229/microsoft-sql-server-logo.svg" alt="mssql" width="40"
+                    height="40" />
+            </a>
+            <a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+                    alt="mysql" width="40" height="40" />
+            </a>
+            <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+                <img src="https://cdn.worldvectorlogo.com/logos/nextjs-2.svg" alt="nextjs" width="40" height="40" />
+            </a>
+            <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+                    alt="nodejs" width="40" height="40" />
+            </a>
+            <a href="https://www.photoshop.com/en" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg"
+                    alt="photoshop" width="40" height="40" />
+            </a>
+            <a href="https://www.php.net" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php"
+                    width="40" height="40" />
+            </a>
+            <a href="https://www.postgresql.org" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+                    alt="postgresql" width="40" height="40" />
+            </a>
+            <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+                    alt="react" width="40" height="40" />
+            </a>
+            <a href="https://reactnative.dev/" target="_blank" rel="noreferrer">
+                <img src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="40" height="40" />
+            </a>
+            <a href="https://sass-lang.com" target="_blank" rel="noreferrer">
+                <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass"
+                    width="40" height="40" />
+            </a>
+            <a href="https://www.adobe.com/products/xd.html" target="_blank" rel="noreferrer">
+                <img src="https://cdn.worldvectorlogo.com/logos/adobe-xd.svg" alt="xd" width="40" height="40" />
+            </a>
+        </p>
+    </div>
+
+    <div class="sm:grid grid-cols-2 gap-0  w-4/5 mx-auto py-5 border-gray-200 ">
+        <p>
+            <img src="https://github-readme-stats.vercel.app/api/top-langs?username=wesley-sinde&show_icons=true&locale=en&layout=compact"
+                alt="wesley-sinde" />
+        </p>
+        <p>&nbsp;
+            <img src="https://github-readme-stats.vercel.app/api?username=wesley-sinde&show_icons=true&locale=en"
+                alt="wesley-sinde" />
+        </p>
+    </div>
+
+
+    <div class=" grid-cols-1 gap-10  center w-4/5 mx-auto py-5 border-gray-200 ">
+        <p>
+            <img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=wesley-sinde&"
+                alt="wesley-sinde" />
+        </p>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+    <div class="text-center py-15 ">
+        <span class="uppercase text-s text-gray-400">
+            Blog
+        </span>
+        <h2 class="text-4xl font-bold py-10">
+            Recent Posts
+        </h2>
+        <p class="m-auto w-4/5 text-gray-500">
+            Maybe you’ve just started learning your first programming language and are looking to get more involved. Or
+            maybe you’ve been dabbling in it for a while and want to kick things up a gear. Wherever you’re coming from,
+            choosing from the range of online coding schools out there can be dizzying.
+
+            Do you go for a university degree in computer science, or would a certification be enough? Does it matter if the
+            certification is from an accredited institution, or would an employer care about that? Should the school focus
+            on technical skills—such as frontend or full-stack development—or more on the soft skills you’ll need to work as
+            a professional programmer? Fear not—we’re here to help you out.
+
+            We’ve rounded up seven of the best online coding schools on the market. The programs these schools offer vary in
+            title from Web Development to Software Engineering, differ in focus from technical to soft skills, can be fully
+            accredited degrees or non-accredited certificates.
+
+            Because different schools approach teaching programming in different ways, we’ll discuss that first. By covering
+            some of the factors and questions you might still be wrestling with as you try to decide, you’ll be well set to
+            examine our top seven picks for online coding schools.
+        </p>
+    </div>
+    <div class="sm:grid  grid-cols-2 w-4/5 m-auto">
+        <div class="flex bg-yellow-700 text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
+                <span class="uppercase text-xs">
+                    The 7 Best Online Coding Schools (and How to Choose One)
+                </span>
+                <h3 class="text-sm font-bold py-10">
+
+                    What kind of online coding school are you really looking for?
+                    <hr>
+                    Accredited vs non-accredited online coding schools
+                    Technical focus: Frontend vs. full-stack online coding schools
+                    Mentorship
+                    Curriculum design
+                    Our top 7 picks for online coding schools:
+
+                    App Academy Online Software Engineering Immersive
+                    CareerFoundry Full-Stack Web Development Program
+                    MIT xPRO Professional Certificate in Coding
+                    Flatiron School Software Engineering Bootcamp
+                    Thinkful Software Engineering Bootcamp
+                    Cornell Web Design and Development Certification Program
+                    BrainStation Web Development Bootcamp
+                </h3>
+
+                <a href=""
+                    class=" uppercase bg-transparent border-2 border-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                    Find Out More
+                </a>
+            </div>
+        </div>
+        <div>
+            <div>
+                <img src="{{ asset('images/school.jpg') }}" alt="" width=100%>
+            </div>
+        </div>
+    </div>
+@endsection
